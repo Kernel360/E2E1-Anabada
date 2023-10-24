71f5cd46e517804269fc6b626763cbfa8589a65a
이윤선 <yoonseon3@gmail.com>
2023-10-24 17:47:11 +0900

    first commit

---
+++ src/main/java/kr/kernel360/anabada/E2e1AnabadaApplication.java
@@ -0,0 +1,13 @@
+package kr.kernel360.anabada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E2e1AnabadaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(E2e1AnabadaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/kernel360/anabada/E2e1AnabadaApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.kernel360.anabada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E2e1AnabadaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
