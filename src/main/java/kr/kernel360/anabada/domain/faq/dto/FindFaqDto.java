c304e5bd291d89fbbc0f90dd93cd479a792563db
이윤선 <yoonseon3@gmail.com>
2023-11-07 17:13:50 +0900

    feat : 작성일시 날짜 포멧팅 추가

--- src/main/java/kr/kernel360/anabada/domain/faq/dto/FindFaqDto.java
+++ src/main/java/kr/kernel360/anabada/domain/faq/dto/FindFaqDto.java
@@ -3,13 +3,13 @@ package kr.kernel360.anabada.domain.faq.dto;
 import java.time.LocalDateTime;
 
 import kr.kernel360.anabada.global.commons.domain.DeletedStatus;
+import kr.kernel360.anabada.global.utils.DateParser;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Builder
 public class FindFaqDto {
 	private Long id;
 
@@ -21,13 +21,13 @@ public class FindFaqDto {
 
 	private String createdBy;
 
-	private LocalDateTime createdDate;
+	private String createdDate;
 
 	public FindFaqDto(Long id, String title, String createdBy, LocalDateTime createdDate) {
 		this.id = id;
 		this.title = title;
 		this.createdBy = createdBy;
-		this.createdDate = createdDate;
+		this.createdDate = DateParser.dateToString(createdDate);
 	}
 
 	public FindFaqDto(Long id, String title, String content, String createdBy, LocalDateTime createdDate) {
@@ -35,16 +35,6 @@ public class FindFaqDto {
 		this.title = title;
 		this.content = content;
 		this.createdBy = createdBy;
-		this.createdDate = createdDate;
-	}
-
-	public FindFaqDto(Long id, String title, String content, DeletedStatus deletedStatus, String createdBy,
-		LocalDateTime createdDate) {
-		this.id = id;
-		this.title = title;
-		this.content = content;
-		this.deletedStatus = deletedStatus;
-		this.createdBy = createdBy;
-		this.createdDate = createdDate;
+		this.createdDate = DateParser.dateTimeToString(createdDate);
 	}
 }
