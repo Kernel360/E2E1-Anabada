d779294b64ebc113f86cf7de26fcee270ff1194b
hodako <kbry12@naver.com>
2023-11-14 15:09:15 +0900

    refactor : configuration 이름 재지정

--- src/main/java/kr/kernel360/anabada/global/kakao/client/KakaoLocationClient.java
+++ src/main/java/kr/kernel360/anabada/global/kakao/client/KakaoLocationClient.java
@@ -1,6 +1,6 @@
 package kr.kernel360.anabada.global.kakao.client;
 
-import kr.kernel360.anabada.global.config.KakaoFeignConfiguration;
+import kr.kernel360.anabada.global.config.KakaoFeignConfig;
 import kr.kernel360.anabada.global.kakao.dto.KakaoPlaceResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.net.URI;
 
-@FeignClient(name = "kakaoLocationClient", configuration = KakaoFeignConfiguration.class)
+@FeignClient(name = "kakaoLocationClient", configuration = KakaoFeignConfig.class)
 public interface KakaoLocationClient {
 
     @GetMapping
