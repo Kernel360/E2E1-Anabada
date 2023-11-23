afe1146e038e7f15b6330aa34e53c2f36f401ea6
이윤선 <yoonseon3@gmail.com>
2023-11-23 11:55:08 +0900

    refactor : 파일 시스템

--- src/main/java/kr/kernel360/anabada/global/FileHandler.java
+++ src/main/java/kr/kernel360/anabada/global/FileHandler.java
@@ -4,35 +4,60 @@ import java.io.File;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import kr.kernel360.anabada.global.error.code.FileErrorCode;
+import kr.kernel360.anabada.global.error.exception.BusinessException;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
 public class FileHandler {
+	private final String imageStoragePath = "images/";
 	public String parseFileInfo(MultipartFile multipartFile, String detailPath) {
-		String contentType = Optional.ofNullable(multipartFile.getContentType())
-			.orElseThrow(() -> new IllegalArgumentException("파일 타입을 찾을 수 없습니다."));
-		if (!(contentType.contains("image/jpeg") || contentType.contains("image/png"))) {
+		String contentType = findContentTypeByFile(multipartFile);
+		if (!isValidImageType(contentType))
 			return null;
-		}
-		String imageName = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyMMddHH24mmss")) + "_" + multipartFile.getOriginalFilename();
-		String imagePath = "../../images/"+ detailPath;
-		File file = new File(imagePath, imageName).getAbsoluteFile();
+		String uniqueImageName = generateUniqueImageName(multipartFile.getOriginalFilename());
+		String imageSavePath = imageStoragePath + detailPath;
+		String filePathString = generateImagePath(imageSavePath, uniqueImageName);
+		transferTo(multipartFile, new File(filePathString));
 
+		return "/images/" + detailPath + "/" + uniqueImageName;
+	}
+	private String findContentTypeByFile(MultipartFile multipartFile) {
+		return Optional.ofNullable(multipartFile.getContentType())
+			.orElseThrow(() -> new BusinessException(FileErrorCode.NOT_FOUND_FILE_TYPE));
+	}
+
+	private boolean isValidImageType(String contentType) {
+		List<String> allowedImageExtensions = List.of("image/jpeg", "image/png");
+		if (!allowedImageExtensions.contains(contentType)) {
+			throw new BusinessException(FileErrorCode.UNSUPPORTED_EXTENSION);
+		}
+		return true;
+	}
+
+	private String generateUniqueImageName(String originalFilename) {
+		return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyMMddHHmmss"))
+			+ "_" + originalFilename;
+	}
+
+	private String generateImagePath(String imageSavePath, String uniqueImageName) {
+		File file = new File(imageSavePath, uniqueImageName).getAbsoluteFile();
+		return file.toString().replace("/build/libs", "");
+	}
+
+	private void transferTo(MultipartFile multipartFile, File file) {
 		try {
 			multipartFile.transferTo(file);
 		} catch (IOException e) {
-			// todo : 추후 exception 타입 변경 필요
-			e.printStackTrace();
-			//throw new IllegalArgumentException("파일시스템 처리 중 에러가 발생하였습니다.");
+			throw new BusinessException(FileErrorCode.FILE_SYSTEM_ERROR);
 		}
-
-		return "/images/" + detailPath + "/" + imageName;
 	}
 
 }
